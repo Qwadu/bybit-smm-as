567cd1b345e1bb465e5b5275c41d5f6c09f48c85
Qwadu <94804436+Qwadu@users.noreply.github.com>
2026-03-26 18:52:10 +0100

    Refactor round_step to use Cython Rounder
    Replaced Decimal-based rounding with Cython-accelerated Rounder for improved performance.

--- src/utils/rounding.py
+++ src/utils/rounding.py
@@ -1,22 +1,18 @@
-from decimal import Decimal
+from functools import lru_cache
+from mm_toolbox.rounding import Rounder, RounderConfig
+
+
+@lru_cache(maxsize=128)
+def _get_rounder(tick_size: float, lot_size: float) -> Rounder:
+    """Cache Rounder instances per tick/lot size pair (Cython-accelerated)."""
+    config = RounderConfig.default(tick_size=tick_size, lot_size=lot_size)
+    return Rounder(config)
+
 
 def round_step(num: float, step: float) -> float:
     """
-    Rounds a float to a given step size
+    Rounds a float to a given step size using mm_toolbox Cython Rounder.
+    Compatible drop-in for the old Decimal-based round_step.
     """
-    num = Decimal(str(num))
-    return float(num - num % Decimal(str(step)))
-
-
-### FIX FOR STEP > 1 ###
-
-# import numpy as np
-# from numba import njit, float64
-
-# @njit(float64(float64, float64), cache=True)
-# def round_step(num: float, step: float) -> float:
-#     """
-#     Rounds a float to a given step size
-#     """
-#     p = int(1/step)
-#     return np.floor(num*p)/p
+    rounder = _get_rounder(step, step)
+    return rounder.bid(num)
